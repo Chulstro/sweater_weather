0547c252685fae79def140c89558edff370ba2e5
Chandler Hulstrom <chulstro11@gmail.com>
2020-09-20 14:10:24 -0600

    Weather service implemented

---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,19 @@
+class OpenWeatherService
+
+  def pull_forecast(location)
+    to_json("onecall?lat=#{location[:lat]}&lon=#{location[:lng]}&exclude=minutely")
+  end
+
+  private
+
+  def to_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://api.openweathermap.org/data/2.5/') do |f|
+      f.params[:appid] = ENV['OPENWEATHER_KEY']
+    end
+  end
+end
