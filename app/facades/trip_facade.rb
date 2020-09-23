44f3560627dda3abeb43aa3ddc7fce3232f794c5
Chandler Hulstrom <chulstro11@gmail.com>
2020-09-22 22:52:05 -0600

    Trip information formatted and endpoint created

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,9 +1,16 @@
 class TripFacade
-  def find_trip
-    #Find endpoints
-    #cry
-    #find weather
-    #cry more
-    #funnel into trip poro
+
+  def self.find_trip(params)
+    map_info = GeocodingService.new.distance_between(params[:origin], params[:destination])
+    latlon = map_info[:route][:locations][1][:latLng]
+    weather_response = OpenWeatherService.new.pull_forecast(latlon)
+    weather_info = weather_response[:hourly][weather_offset(map_info[:route][:time])]
+    Trip.new(weather_info, map_info)
+  end
+
+  private
+
+  def self.weather_offset(time)
+    (time/60/60.to_f).round
   end
 end
