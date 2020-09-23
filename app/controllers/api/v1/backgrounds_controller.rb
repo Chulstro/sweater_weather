826d9ea11475c6be629db2180389e5b9f2d1f454
Chandler Hulstrom <chulstro11@gmail.com>
2020-09-23 08:28:15 -0600

    Added api sad path for road trip request

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
     image_object = BackgroundFacade.find_image(params[:location])
-    # image_object = PixabayService.new.city_image(params[:location])
     render json: BackgroundSerializer.new(image_object)
   end
 end
